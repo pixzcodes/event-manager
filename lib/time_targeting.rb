666896916b8028268aeb5accc9c189b800f62e1a
Pixz <tinycactusking@gmail.com>
2022-12-11 14:33:28 -0500

    Add top 3 days of the week display in terminal, project effectivly finished

--- lib/time_targeting.rb
+++ lib/time_targeting.rb
@@ -9,22 +9,48 @@ contents = CSV.open(
   header_converters: :symbol
 )
 hours_hash = Hash.new
-
+weekday_hash = Hash.new
 contents.each do |row|
   regtime = row[:regdate]
-
-  hour = DateTime.strptime(regtime, "%m/%d/%y %k:%M").hour
+  time_obj = DateTime.strptime(regtime, "%m/%d/%y %k:%M")
+  hour = time_obj.hour
+  day = time_obj.wday
   if hours_hash.key?(hour)
     hours_hash[hour] += 1
   else
     hours_hash[hour] = 1
   end
+  if weekday_hash.key?(day)
+    weekday_hash[day] += 1
+  else
+    weekday_hash[day] = 1
+  end
 end
 hours_hash = hours_hash.sort_by {|hour, count| count}
 puts "The top 3 best hours:"
-hours_hash[-3..].each do |hour, count|
-  puts hour
-end
+hours_hash[-3..].each {|hour, count| puts hour}
 puts "Full List:"
 puts "[Hour, Count]"
 p hours_hash
+weekday_hash = weekday_hash.sort_by {|day, count| count}
+puts "The top 3 best days:"
+weekday_hash[-3..].each do |day, count|
+  case day
+  when 0
+    puts "Sunday"
+  when 1
+    puts "Monday"
+  when 2 
+    puts "Tuesday"
+  when 3
+    puts "Wednesday"
+  when 4 
+    puts "Thursday"
+  when 5 
+    puts "Friday"
+  when 6
+    puts "Saturday"
+  else
+    puts "Error"
+  end
+end
