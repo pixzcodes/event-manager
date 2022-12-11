9ffc7658390493292e97d9f67c6934a21ce8d6b6
Pixz <tinycactusking@gmail.com>
2022-12-11 14:11:56 -0500

    Add display for top three best hours to advertise

--- lib/time_targeting.rb
+++ lib/time_targeting.rb
@@ -8,13 +8,23 @@ contents = CSV.open(
   headers: true,
   header_converters: :symbol
 )
+hours_hash = Hash.new
 
 contents.each do |row|
   regtime = row[:regdate]
-  
-  puts "reg time:"
-  p regtime
-  parse_time = DateTime.strptime(regtime, "%m/%d/%y %k:%M")
-  puts "parsed time:"
-  p parse_time
+
+  hour = DateTime.strptime(regtime, "%m/%d/%y %k:%M").hour
+  if hours_hash.key?(hour)
+    hours_hash[hour] += 1
+  else
+    hours_hash[hour] = 1
+  end
 end
+hours_hash = hours_hash.sort_by {|hour, count| count}
+puts "The top 3 best hours:"
+hours_hash[-3..].each do |hour, count|
+  puts hour
+end
+puts "Full List:"
+puts "[Hour, Count]"
+p hours_hash
